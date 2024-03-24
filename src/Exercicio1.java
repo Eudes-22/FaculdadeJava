a21e19fa428cfd4eda789f1cf5a150feafa0f39e
Eudes Pereira <eudes.pereira22@gmail.com>
2024-03-24 14:04:58 -0300

    adiciona exercicio 1

--- src/Exercicio1.java
+++ src/Exercicio1.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
 public class Exercicio1 {
+    static public class Aluno {
+        int nota1, nota2, nota3;
 
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        public double calculaMedia() {
+            double media = (nota1 + nota2 + nota3)/3;
+            return media;
         }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Aluno aluno = new Aluno();
+        System.out.printf("Digite a nota 1: ");
+        aluno.nota1 = scanner.nextInt();
+        System.out.printf("Digite a nota 2: ");
+        aluno.nota2 = scanner.nextInt();
+        System.out.printf("Digite a nota 3: ");
+        aluno.nota3 = scanner.nextInt();
+
+        double media = aluno.calculaMedia();
+        System.out.printf("media das notas: " + media);
     }
 }
